a33d0f74485ec24a15346bc6e0212e000a1d5023
Ian Barros <ian.barros@sinqia.com.br>
2020-06-24 23:59:32 -0300

    Pratica completa

--- src/app/pessoa.service.ts
+++ src/app/pessoa.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Pessoa } from './model/pessoa.model';
+import { Storage } from '@ionic/storage';
+import { DatePipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PessoaService {
 
-  constructor() { }
+  private pessoas = [] as Pessoa[];
+
+  constructor(private storage: Storage, private datePipe: DatePipe) { }
+
+  async insertPessoa(model: any){
+    let pessoa: Pessoa;
+    pessoa.id = model.id;
+    pessoa.idade = model.idade;
+    pessoa.nome = model.nome;
+    pessoa.saudeMental = model.saudeMental;
+    pessoa.racaCor = model.racaCor;
+    this.pessoas.push(pessoa);
+    await this.storage.set("pessoas",this.pessoas);
+  }
+  async getPessoaById(id: number){
+    let arrayPessoas = await this.storage.get("pessoas");
+    console.log('PESSOA COM O ID PESQUISADO:\n' + arrayPessoas.find(x => x.id === id));
+  }
+  async getPessoaByRaca(racaCor: string){
+    let arrayPessoas = await this.storage.get("pessoas");
+    let pessoasPorRaca = [];
+    arrayPessoas.forEach(p => {
+      if (p.racaCor === racaCor) {pessoasPorRaca.push(p)};
+    });
+    console.log('PESSOAS COM A COR/RAÇA PESQUISADA:\n' + pessoasPorRaca);
+  }
+  async getPessoaBySaudeMental(saudeMental: number){
+    let arrayPessoas = await this.storage.get("pessoas");
+    let pessoasPorSaudeMental = [];
+    arrayPessoas.forEach(p => {
+      if (p.saudeMental === saudeMental) {pessoasPorSaudeMental.push(p)};
+    });
+    console.log('PESSOAS COM A SAÚDE MENTAL PESQUISADA:\n' + pessoasPorSaudeMental);
+  }
 }
--- src/app/saude-mental/saude-mental.page.ts
+++ src/app/saude-mental/saude-mental.page.ts
@@ -1,19 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Pessoa } from '../model/pessoa.model';
+import { PessoaService } from '../pessoa.service';
 
 @Component({
   selector: 'app-saude-mental',
   templateUrl: './saude-mental.page.html',
   styleUrls: ['./saude-mental.page.scss'],
 })
+
+
 export class SaudeMentalPage implements OnInit {
-  model = {} as Pessoa;
+  model = {} as any;
   idBusca: number; 
   saudeMentalBusca: number;
+  racaBusca: string;
 
-  constructor() { }
+  constructor(private pessoaService: PessoaService) { }
 
   ngOnInit() {
   }
-
+  buscarPorRaca(){
+    this.pessoaService.getPessoaByRaca(this.racaBusca);
+  }
+  buscarPorId(){
+    this.pessoaService.getPessoaById(this.idBusca);
+  }
+  buscarPorSaudeMental(){
+    this.pessoaService.getPessoaBySaudeMental(this.saudeMentalBusca);
+  }
+  insertPessoa(){
+    this.pessoaService.insertPessoa(this.model);
+  }
 }
